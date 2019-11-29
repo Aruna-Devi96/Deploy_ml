454a901519b4f118b350d6b3060cd91f27a3c566
Aruna-Devi96 <58296923+Aruna-Devi96@users.noreply.github.com>
2019-11-29 11:17:29 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -1,18 +1,20 @@
-import pickle
-import pandas as pd
 import numpy as np
-print("HI")
+import pandas as pd
+import pickle
 
-dataset = pd.read_csv('salary_prediction.csv')
+dataset = pd.read_csv(r'D:\swarna\DEPLOY_ML\salary_prediction.csv')
+
 X = dataset.iloc[:,:3]
+
 y = dataset.iloc[:,-1]
 
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 
-regressor.fit(X,y)
+regressor.fit(X, y)
 
 pickle.dump(regressor, open('model.pkl','wb'))
 
 model = pickle.load(open('model.pkl','rb'))
 print(model.predict([[2,9,6]]))
+                        
--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'Experience':2, 'Interview Score':9, 'Test Score':6})
+r = requests.post(url,json={'experience':2, 'test_score':9, 'interview_score':6})
 
 print(r.json())
